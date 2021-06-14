3217235d4c01b9a192180cea064234f0042a277b
lachoneus <6295215+lachoneus@users.noreply.github.com>
2021-06-13 20:33:35 -0600

    Update rmvlib_install.sh
    Made the cross-tools install automatic.  Added a check to make sure that the necessary packages are install in cygwin.

--- rmvlib_install.sh
+++ rmvlib_install.sh
@@ -6,17 +6,7 @@
 
 #During installation, add the following additional packages on top of the default packages that will be installed (latest versions):
     
-    # make, gcc-core, git, libmpfr4, libmpc3, doxygen
-
-#Download and install the m68k-atari-mint cross compiler tools  from the following link near the top of the page (name should look something like ' m68k-atari-mint-base-20200501-cygwin32.tar.xz'):
-
-    # https://tho-otto.de/crossmint.php
-
-#Copy this downloaded .TAR file to your home folder in cygwin.  This is usually located at C:\cygwin\home\<user name>
-
-#Open cygwin and extract into the root of cygwin with the following command
-
-    # tar -xvf ./m68k-atari-mint-base-20200501-cygwin32.tar.xz -C /
+    # wget, make, gcc-core, git, libmpfr4, libmpc3, doxygen
 
 #Clone the git repository for install script with the following command
 
@@ -34,13 +24,41 @@
 #Script
 
 #setup our folders for our tools and build environment path
-RED='\033[0;31m'
-NC='\033[0m' # No Color
 INSTALLDIRECTORYNAME='Jaguar'
 INSTALLPATH="/home/$USER/$INSTALLDIRECTORYNAME"
 
+i=0; n=0; progs=(wget make gcc git doxygen);
+for p in "${progs[@]}"; do
+    if hash "$p" &>/dev/null
+    then
+        echo "$p is installed"
+        sleep 0.5
+        let c++
+    else
+        echo "$p is not installed"
+        sleep 0.5
+        let n++
+    fi
+done
+
+if [ "$n" -gt "0" ]
+then
+    echo "One or more necessary applications has not been install through the Cygwin setup executable. (Read messages above to see which applications aren't installed)."
+    echo "Run Cygwin setup again, and make sure the follow packages are installed:"
+    echo " "
+    echo "wget, make, gcc-core, git, libmpfr4, libmpc3, doxygen"
+    exit
+
+else
+
+#download and install m-64k-atari-mint cross compiler tools
+echo  " Downloading and Installing Cross Compiler "
+wget --no-check-certificate https://tho-otto.de/download/mint/m68k-atari-mint-base-20200501-cygwin32.tar.xz
+tar -xvf ./m68k-atari-mint-base-20200501-cygwin32.tar.xz -C /
+rm -rvf ./m68k-atari-mint-base-20200501-cygwin32.tar.xz
+
 #setup our folders for our tools and build environment path
-echo  "\n${RED}Adding Tools Directory${NC}\n"
+echo  " Adding Tools Directory "
 
 mkdir -vp $INSTALLPATH
 mkdir -vp $INSTALLPATH/bin
@@ -50,63 +68,65 @@ mkdir -vp $INSTALLPATH/lib/lib
 mkdir -vp $INSTALLPATH/lib/include
 mkdir -vp $INSTALLPATH/src
 
-echo  "\n${RED}Copy Temporary Assets Directory${NC}\n"
+echo  " Copy Temporary Assets Directory "
 cp -vr ./assets $INSTALLPATH
 cd $INSTALLPATH/
 
-echo  "\n${RED}Setting Environment Variable JAGPATH${NC}\n"
+echo  " Setting Environment Variable JAGPATH "
 export JAGPATH=$INSTALLPATH #export for the session the script is running so libraries install correctly
 
 echo "export JAGPATH=/home/$USER/Jaguar" | tee -a $HOME/.bashrc #make environment variable persistent across sessions.
 
-echo  "\n${RED}Downloading RMAC/RLN Source From GIT Repositories${NC}\n"
+echo  " Downloading RMAC/RLN Source From GIT Repositories "
 cd $INSTALLPATH/src
 git clone http://shamusworld.gotdns.org/git/rmac
 git clone http://shamusworld.gotdns.org/git/rln
-echo  "\n${RED}Downloading jlibc/rmvlib Libraries From GIT Repositories${NC}\n"
+echo  " Downloading jlibc/rmvlib Libraries From GIT Repositories "
 git clone https://github.com/sbriais/jlibc.git
 git clone https://github.com/sbriais/rmvlib.git
 
-echo "${RED}\n\nBegin Building sources\n\n${NC}"
+echo "  Begin Building sources  "
 
 #modify and build rmac 2.0.0
-echo  "\n${RED}Building RMAC${NC}\n"
+echo  " Building RMAC "
 cd rmac
 make
 cp -vr rmac $INSTALLPATH/bin/mac #renaming to make more compatible with sebs makefiles
 cd $INSTALLPATH/src
 
 #patching and building rln
-echo  "\n${RED}Building RLN${NC}\n"
+echo  " Building RLN "
 cd rln
 make
 cp -vr rln $INSTALLPATH/bin/aln #renaming to make more compatible with sebs makefiles
 cd $INSTALLPATH/src
 
 #modify and build jlibc
-echo  "\n${RED}Building JLIBC${NC}\n"
+echo  " Building JLIBC "
 cd jlibc
 make
 make install
 cd $INSTALLPATH/src
 
 #modify and build rmvlib
-echo  "\n${RED}Building RMVLIB${NC}\n"
+echo  " Building RMVLIB "
 cd rmvlib
 make
 make install
 cd $INSTALLPATH/src
 
 #copy libgcc.a from m68k-mint-atari tools
-echo  "\n${RED}copy libgcc.a from m68k-mint-atari tools into lib folder${NC}\n"
+echo  " copy libgcc.a from m68k-mint-atari tools into lib folder "
 cd $INSTALLPATH/lib/lib
 find /usr/lib/gcc/m68k-atari-mint/ -type f -name "libgcc.a" -exec cp {} ./ \;
 cd $INSTALLPATH
 
-echo  "\n${RED}copy example program${NC}\n"
+echo  " copy example program "
 mv -v ./assets/generic_example ./example_programs/
 rm -rv ./assets
 
-echo  "\n${RED}DONE!${NC}\n"
+echo  " DONE! "
 
-echo  "\n${RED}RESTART CYGWIN BEFORE BUILDING EXAMPLE PROGRAM!${NC}\n"
+echo  " RESTART CYGWIN BEFORE BUILDING EXAMPLE PROGRAM! "
+
+fi
